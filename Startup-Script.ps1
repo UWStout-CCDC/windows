4a0013ba57e7803379ece6078cdb8064f5d60c75
Rexantor96 <123572490+Rexantor96@users.noreply.github.com>
2025-10-03 20:09:42 -0500

    added banners

--- Startup-Script.ps1
+++ Startup-Script.ps1
@@ -6,6 +6,12 @@ Import-Module -Name BitsTransfer
 $Host.UI.RawUI.ForegroundColor = "DarkGreen"
 $Host.UI.RawUI.BackgroundColor = "Black"
 
+Write-Host " _       ___           __                      ________                          "
+Write-Host "| |     / (_)___  ____/ /___ _      _______   / ____/ /__  ____ _____  ___  _____"
+Write-Host "| | /| / / / __ \/ __  / __ \ | /| / / ___/  / /   / / _ \/ __ `/ __ \/ _ \/ ___/"
+Write-Host "| |/ |/ / / / / / /_/ / /_/ / |/ |/ (__  )  / /___/ /  __/ /_/ / / / /  __/ /    "
+Write-Host "|__/|__/_/_/ /_/\__,_/\____/|__/|__/____/   \____/_/\___/\__,_/_/ /_/\___/_/  "
+
 $site = "UWStout-CCDC/windows"
 
 # Clear startup items from registry
