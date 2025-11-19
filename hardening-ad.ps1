07eeb8760ec95ea1b8bacc46c911f418be1d9fe8
censr-ps1 <ethanjcoulthard@gmail.com>
2025-11-18 20:08:39 -0600

    updated to main

--- hardening-ad.ps1
+++ hardening-ad.ps1
@@ -13,20 +13,23 @@ Write-Host "| | /| / / / __ \/ __  / __ \ | /| / / ___/  / /   / / _ \/ __ `/ __
 Write-Host "| |/ |/ / / / / / /_/ / /_/ / |/ |/ (__  )  / /___/ /  __/ /_/ / / / /  __/ /    "
 Write-Host "|__/|__/_/_/ /_/\__,_/\____/|__/|__/____/   \____/_/\___/\__,_/_/ /_/\___/_/  "
 
-###$site = "UWStout-CCDC/windows"
+$site = "UWStout-CCDC/windows"
+Create-Item -Path "C:\CCDC" -ItemType Directory
+Create-Item -Path "C:\CCDC\tools-Windows" -ItemType Directory
 
-# Clear startup items from registry
+## Clear persistence and document it ##
+
+# Registry persistence
 $startupRegistryPaths = @(
     "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run",
     "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce",
-    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run",
-    "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows\Run",
-    
-    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run",
     "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run",
     "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce",
     "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Run",
     "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\RunOnce",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run",
+    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run",
+    "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows\Run",
     "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Windows\Run"
 )
 
@@ -36,14 +39,14 @@ foreach ($path in $startupRegistryPaths) {
     if ($items) {
         $items.PSObject.Properties | ForEach-Object {
             if ($_.Name -ne "PSPath" -and $_.Name -ne "PSParentPath" -and $_.Name -ne "PSChildName" -and $_.Name -ne "PSDrive" -and $_.Name -ne "PSProvider") {
+                $items >> "C:\CCDC\persistence-registry.txt"
                 Remove-ItemProperty -Path $path -Name $_.Name -ErrorAction SilentlyContinue
             }
         }
     }
 }
-Write-Host "Done clearing startup items from registry"
 
-# Clear startup items from startup folders
+# Start menu persistence
 $startupFolders = @(
     "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup",
     "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
@@ -52,16 +55,14 @@ $startupFolders = @(
 foreach ($folder in $startupFolders) {
     Write-Host "Clearing startup items from $folder"
     Get-ChildItem -Path $folder | ForEach-Object {
+        $_.FullName >> "C:\CCDC\persistence-startup.txt"
         Remove-Item -Path $_.FullName -Force -ErrorAction SilentlyContinue
     }
 }
-Write-Host "Done clearing startup items"
 
 # Clear scheduled tasks
 Write-Host "Clearing scheduled tasks..."
 Get-ScheduledTask | Where-Object { $_.TaskPath -notlike "\Microsoft\*" } | ForEach-Object {
+    $_.TaskName >> "C:\CCDC\persistence-schtasks.txt"
     Unregister-ScheduledTask -TaskName $_.TaskName -Confirm:$false
 }
-Write-Host "Done clearing scheduled tasks"
-
-
