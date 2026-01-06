f9d523486f9bebf049b30c98977dee60547d2691
Rexantor96 <willworthin@outlook.com>
2026-01-06 10:42:59 -0600

    Added password changes and directories

--- hardening-ad.ps1
+++ hardening-ad.ps1
@@ -66,3 +66,61 @@ Get-ScheduledTask | Where-Object { $_.TaskPath -notlike "\Microsoft\*" } | ForEa
     $_.TaskName >> "C:\CCDC\persistence-schtasks.txt"
     Unregister-ScheduledTask -TaskName $_.TaskName -Confirm:$false
 }
+
+# Prompt for new administrator password and confirmation
+try {
+    do {
+        $newAdminPassword = Read-Host -AsSecureString "Enter new password for the local administrator account"
+        $confirmAdminPassword = Read-Host -AsSecureString "Confirm new password for the local administrator account"
+
+        $newAdminPasswordPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($newAdminPassword))
+        $confirmAdminPasswordPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($confirmAdminPassword))
+
+        if ($newAdminPasswordPlain -ne $confirmAdminPasswordPlain) {
+            Write-Host "Passwords do not match. Please try again."
+        }
+    } while ($newAdminPasswordPlain -ne $confirmAdminPasswordPlain)
+
+    # Change local administrator password
+    $adminAccount = Get-LocalUser -Name "Administrator"
+    Set-LocalUser -Name $adminAccount -Password $newAdminPassword
+    Write-Host "--------------------------------------------------------------------------------"
+    Write-Host "Administrator password changed."
+    Write-Host "--------------------------------------------------------------------------------"
+} catch {
+    Write-Host "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    Write-Host "An error occurred while changing the administrator password: $_"
+    Write-Host "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+}
+
+
+# Rotate Kerberos account password
+try {
+    $count = 0;
+    while ($count -lt 3) {
+        Write-Host "Rotating Kerberos account password..."
+        $letterNumberArray = @('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8','9','0', '!', '@', '#', '$', '%', '^', '&', '*')
+        for(($counter=0); $counter -lt 20; $counter++)
+        {
+        $randomCharacter = get-random -InputObject $letterNumberArray
+        $password = $randomString + $randomCharacter
+        }
+
+        $krbtgt = Get-LocalUser -Name "krbtgt"
+        Set-LocalUser -Name $krbtgt -Password $password
+    }
+    
+    Write-Host "--------------------------------------------------------------------------------"
+    Write-Host "Kerberos account password rotated successfully."
+    Write-Host "--------------------------------------------------------------------------------"
+} catch {
+    Write-Host "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    Write-Host "An error occurred while rotating Kerberos password: $_"
+    Write-Host "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+}
+
+# Create directories
+$ccdcPath = "C:\CCDC"
+mkdir $ccdcPath 
+mkdir "$ccdcPath\DNS" 
+mkdir "C:\CCDC\tools-Windows" 
